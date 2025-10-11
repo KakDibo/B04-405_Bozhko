ec6857f719d614aa98a1fb8ad6db53f0a5f19ba0
Makar <makarbozko@gmail.com>
2025-10-11 16:50:01 +0300

    first commit

---
+++ C++/GOIDA/GOIDA/GGOIDA.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long double a, c;
+    float b;
+    cin >> a;
+    b = float(a);
+    long double c = (b - a);
+    cout << c;
+}
+
+
+
+
